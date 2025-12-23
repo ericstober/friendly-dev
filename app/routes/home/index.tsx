12d8d03bfb6c6794bcf89c1d4b8365ade166aefb
ericstober <erictstober@gmail.com>
2025-12-23 14:29:26 -0600

    complete initial setup and create routes

---
+++ app/routes/home/index.tsx
@@ -0,0 +1,9 @@
+import type { Route } from "./+types/index";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "The Friendly Dev | Welcome" }, { name: "description", content: "Custom website development" }];
+}
+
+export default function Home() {
+  return <section>My App</section>;
+}
