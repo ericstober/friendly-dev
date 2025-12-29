0bf7fdc52ac037b2b552e97935639c220ef5ddfc
ericstober <erictstober@gmail.com>
2025-12-29 13:53:09 -0600

    create Hero component

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,9 +1,14 @@
 import type { Route } from "./+types/index";
+import Hero from "~/components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "The Friendly Dev | Welcome" }, { name: "description", content: "Custom website development" }];
 }
 
 export default function Home() {
-  return <section>My App</section>;
+  return (
+    <section>
+      <Hero name='Eric' />
+    </section>
+  );
 }
