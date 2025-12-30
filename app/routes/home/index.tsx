45dbe6f76b6fabeaffba2b6ee89c5f08d739030a
ericstober <erictstober@gmail.com>
2025-12-30 15:35:57 -0600

    setup home and main layouts and routes. home layout is same as main but has Hero component on top. Changed each page section to a fragment since the layouts use a section, this way we don't end up with two nested sections

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,14 +1,9 @@
 import type { Route } from "./+types/index";
-import Hero from "~/components/Hero";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "The Friendly Dev | Welcome" }, { name: "description", content: "Custom website development" }];
 }
 
 export default function Home() {
-  return (
-    <section>
-      <Hero name='Eric' />
-    </section>
-  );
+  return <section>Home Page</section>;
 }
