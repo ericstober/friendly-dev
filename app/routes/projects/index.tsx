a339445022be9f66245b55ada9e845956fd754d3
ericstober <erictstober@gmail.com>
2026-01-01 19:10:36 -0600

    display loader data/project data on projects page. Create ProjectCard component to display card for each project

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
+import ProjectCard from "~/components/ProjectCard";
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch("http://localhost:8000/projects");
@@ -10,11 +11,16 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ projects:
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
 
   return (
     <section>
       <h2 className='text-3xl text-white font-bold mb-8'>🚀 Projects</h2>
+
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </section>
   );
 };
